011ee4999ad6204bc2d6e75d60d0b15071987c27
9208 <9208@dev198.com>
2020-01-17 11:09:35 +0800

    level1：buildType配置打包方式

--- app/src/main/java/com/zhou/enjoygradlehank/MainActivity.kt
+++ app/src/main/java/com/zhou/enjoygradlehank/MainActivity.kt
@@ -2,11 +2,13 @@ package com.zhou.enjoygradlehank
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvBuildType.text = BuildConfig.BUILD_TYPE
     }
 }
